c4345caa4b64b1ee44041536ff8228f776c00456
Eugenee丶 <48148830+EEEugene@users.noreply.github.com>
2023-05-21 11:19:50 +0800

    Update worldlistwea.h

--- worldlistwea.h
+++ worldlistwea.h
@@ -5,13 +5,10 @@
 #include <QLabel>
 #include <QMap>
 #include <QMainWindow>
-#include "weatherData.h"
 #include<QNetworkAccessManager>
 #include<QNetworkReply>
 #include<QListWidget>
-//#include "dialog.h"
 #include "citywithtemp.h"
-//#include "mainwindow.h"
 
 namespace Ui {
 class WorldListWea;
@@ -24,51 +21,44 @@ class WorldListWea : public QMainWindow
 public:
     explicit WorldListWea(QWidget *parent = nullptr);
     ~WorldListWea();
-//    Dialog* dlg = new Dialog;
-    QVector<QString> cityList;
+    QString updateCity;
+    QString updateTemp;
+    QString updateType;
+    QList<QString> cityList;
+    QList<QString> tempList;
+    QList<QString> typeList;
+    QList<QString> clickedCity;
+
 protected:
     void contextMenuEvent(QContextMenuEvent* event);
     void mousePressEvent(QMouseEvent* event);
     void mouseMoveEvent(QMouseEvent* event);
-    void getWeatherInfo(QString cityName);
-    void getWorldWeather(QString wcity);
-    void parseJson(QByteArray& byteArray);
-    void updateUI();
-    void appearButton();
-//    void initWidget();
-//    void appearList();
+    void setState();
+    void asiaCity();
+    void euCity();
+    void afCity();
+    void naCity();
+    void oaCity();
 
 signals:
     void BackButton();
     void setData(QString text);
+    void sendIsland(QString);
 
 private slots:
-//    void on_Refresh_clicked();
-    void on_Back_clicked();
-    void on_EuBTn_clicked();
-    void on_AfBtn_clicked();
-    void on_NaBTn_clicked();
-    void on_OaBTn_clicked();
-//    void on_pushButton_clicked();
     void on_pushButton_2_clicked();
-
-    void on_AsiaBtn_clicked();
-    void on_listWidget_itemDoubleClicked();
-    void on_listWidget_itemClicked();
-    void on_hide_clicked();
-    void getCity(QString);
-    void GetData(QString);
+    void on_listWidget_itemDoubleClicked(QListWidgetItem *item);
+    void on_stateWidget_itemDoubleClicked();
+    void on_listBtn_clicked();
+    void getTemp(QString temp,QString city,QString type);
 
 private:
-    void onReplied(QNetworkReply* reply);
     Ui::WorldListWea *ui;
     QMenu* mExitMenu;   // 退出菜单
     QAction* mExitAct;  // 菜单项（退出）
     QPoint mOffset;     //窗口移动时，鼠标与左上角的偏移
     QNetworkAccessManager* mNetAccessManager;
-    void AddItem(QString str1,QString str2,const QString& pic);
-    Today mToday;
-    Day mDay[6];
+    void AddItem(QString city, QString temp, QString pic);
 };
 
 #endif // WORLDLISTWEA_H
